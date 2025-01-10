c230582f645c2a2a0b364cae7d2708affa2e19f9
zachuri <punsalang.zachary@gmail.com>
2025-01-09 21:09:23 -0800

    More updates to header interface

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,18 +1,18 @@
 "use client";
 
 import { useTimerStore } from "~/store/useTimerStore";
-import SessionsUI from "./sessionsUI/SessionsUI";
+import SessionsUI from "./SessionsUI";
 
 function Timer() {
   const { isWorking, isRunning } = useTimerStore();
 
   return (
-    <div className="flex h-[15vh] w-full items-center justify-between px-[5vw]">
+    <div className="flex h-[15vh] w-full items-center justify-between px-[5vw] pt-10">
       <div className="font-regular text-9xl">
         <SessionsUI />
       </div>
       <div className="flex flex-col items-center justify-center text-right">
-        <p className="font-regualr text-5xl">
+        <p className="text-5xl font-bold">
           {isRunning ? (isWorking ? "Focus" : "Break") : "BeFoucsed"}
         </p>
       </div>
